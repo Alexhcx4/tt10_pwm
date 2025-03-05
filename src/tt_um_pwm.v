39e4cf6ffe4e0b205af993452f6442cf73560854
Alexhcx4 <alexhdzc04@gmail.com>
2025-03-05 00:23:17 -0600

    Update and rename project.v to tt_um_pwm.v

--- src/tt_um_pwm.v
+++ src/tt_um_pwm.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_pwm (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -16,12 +16,20 @@ module tt_um_example (
     input  wire       rst_n     // reset_n - low to reset
 );
 
+    pwm c (
+        .clk(ui_in[0]),
+        .rst(ui_in[1]),
+        .sw(ui_in[5:2]),
+        .pwm(uo_out[0])
+    );
+    
+    
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+    assign uo_out [1:7] = 7'b0000000;  // Example: ou_out is the sum of ui_in and uio_in
+    assign uio_out [7:0] = 8'b00000000;
+    assign uio_oe [7:0] = 8'b00000000;
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+    wire _unused = &{ena, clk, rst_n, ui_in[7:6], uio_in[7:0], uio_out[7:0] };
 
 endmodule
